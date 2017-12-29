ad8ca5afba11c0beb76973ad45d275a7ad6357bc
Nathan Patnam <patnamnv@email.wofford.edu>
2017-12-29 12:18:12 -0600

    Going from Postgress to SQLlite

--- static/login.js
+++ static/login.js
@@ -1,20 +1,3 @@
 
-function login() {
-        var user = $('#txtUsername').val();
-        var pass = $('#txtPassword').val();
-        $.ajax({
-            url: '/login',
-            data: $('form').serialize(),
-            type: 'POST',
-            success: function(response) {
-                e.preventDefault();
-                console.log(response;
-            },
-            error: function(error) {
-                e.preventDefault();
-                console.log(error);
-            }
-        });
 
-}
 
